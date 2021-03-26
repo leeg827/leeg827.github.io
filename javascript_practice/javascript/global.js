0e985ab0ff1689d9994d3e3a2e4588841d45da10
leeg827 <leeg827@newschool.edu>
2021-03-26 02:36:04 -0700

    finished product

--- javascript_practice/javascript/global.js
+++ javascript_practice/javascript/global.js
@@ -1,5 +1,11 @@
 let container = document.querySelector('#container')
 let button = document.querySelector('button')
+let count = document.querySelector('#counter')
+let redButton = document.querySelector('#red')
+let blueButton = document.querySelector('#blue')
+let yellowButton = document.querySelector('#yellow')
+let clear = document.querySelector('#clear')
+
 button.addEventListener('click',function(){
    let circle = document.createElement('div');
    circle.classList.add('circle');
@@ -7,15 +13,48 @@ button.addEventListener('click',function(){
    circle.style.top=(100*Math.random()) + '%';
    circle.innerHTML='<div></div>';
    container.appendChild(circle);
+
+   let images = container.querySelectorAll('.circle');
+   let amount = images.length;
+   count.innerHTML = amount;
 });
+
 console.log('hello world')
 
 container.addEventListener ('click',function(event){
-    if(event.target.classList.contains('circle')){
+    if(event.target.classList == 'circle'){
         event.target.remove();
-    }});
+        let circles= container.querySelectorAll('.circle');
+        let amount = circles.length;
+        count.innerHTML = amount;
+}});
 
-let circles = document.querySelectorAll('.circle')
-circles.forEach(function(circle){
-    console.log(circles.length);
+redButton.addEventListener ('click',function(){
+    let red = document.querySelector('#red').style.backgroundColor;
+    let circles = document.querySelectorAll('.circle');
+    circles.forEach(function(circle){
+        circle.style.backgroundColor = 'red';
+    })
 });
+
+blueButton.addEventListener ('click',function(){
+    let blue = document.querySelector('#red').style.backgroundColor;
+    let circles = document.querySelectorAll('.circle');
+    circles.forEach(function(circle){
+        circle.style.backgroundColor = 'blue';
+    })
+});
+
+yellowButton.addEventListener ('click',function(){
+    let yellow = document.querySelector('#yellow').style.backgroundColor;
+    let circles = document.querySelectorAll('.circle');
+    circles.forEach(function(circle){
+        circle.style.backgroundColor = 'yellow';
+    })
+});
+
+clear.addEventListener('click',function(){
+    container.innerHTML='';
+    count.innerHTML='0';
+})
+
