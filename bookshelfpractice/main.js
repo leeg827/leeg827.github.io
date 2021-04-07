8dce92a2dfa5b3df62991a558a590e935364a3d9
leeg827 <leeg827@newschool.edu>
2021-04-06 20:35:55 -0700

    additional stuff

--- bookshelfpractice/main.js
+++ bookshelfpractice/main.js
@@ -1,47 +1,36 @@
-/* globals require */
+
 console.log("Hello, Airtable");
 
-// load the airtable library, call it "Airtable"
 var Airtable = require("airtable");
 console.log(Airtable);
 
-// use the airtable librar to get a variable that represents one of our bases
 var base = new Airtable({ apiKey: "key4Jc4fqyYdDCwcd" }).base(
   "appiqsEYrTVSOEttD"
 );
 
-//get the "books" table from the base, select ALL the records, and specify the functions that will receive the data
 base("books").select({}).eachPage(gotPageOfBooks, gotAllBooks);
 
-// an empty array to hold our book data
 const books = [];
 
-// callback function that receives our data
 function gotPageOfBooks(records, fetchNextPage) {
   console.log("gotPageOfBooks()");
-  // add the records from this page to our books array
   books.push(...records);
-  // request more pages
   fetchNextPage();
 }
 
-// call back function that is called when all pages are loaded
 function gotAllBooks(err) {
   console.log("gotAllBooks()");
 
-  // report an error, you'd want to do something better than this in production
   if (err) {
     console.log("error loading books");
     console.error(err);
     return;
   }
 
-  // call functions to log and show the books
   consoleLogBooks();
   showBooks();
 }
 
-// just loop through the books and console.log them
 function consoleLogBooks() {
   console.log("consoleLogBooks()");
   books.forEach((book) => {
@@ -49,12 +38,52 @@ function consoleLogBooks() {
   });
 }
 
-// loop through the books, create an h2 for each one, and add it to the page
 function showBooks() {
   console.log("showBooks()");
   books.forEach((book) => {
-    const h2 = document.createElement("h2");
+    const h1 = document.createElement("h1");
     h2.innerText = book.fields.title;
-    document.body.appendChild(h2);
+    document.body.appendChild(h1);
   });
+}
+
+
+function showBooks() {
+  console.log("showBooks()");
+
+  const shelf = document.getElementById("shelf");
+
+  books.forEach((book) => {
+    const div = document.createElement("div");
+    div.innerText = book.fields.title;
+    div.classList.add("book-spine");
+    div.addEventListener("click", () => {
+      showBook(book, div);
+    });
+    shelf.appendChild(div);
+  });
+}
+
+function showBook(book, div) {
+  console.log("showBook()", book);
+
+  const bookDetail = document.getElementById("book-detail");
+
+  bookDetail.getElementsByClassName("title")[0].innerText = book.fields.title; //
+  bookDetail.getElementsByClassName("description")[0].innerText =
+    book.fields.description;
+  bookDetail.getElementsByClassName("more")[0].href = book.fields.more;
+  bookDetail.getElementsByClassName("cover-image")[0].src =
+    book.fields.cover_image[0].url;
+
+
+  const shelf = document.getElementById("shelf");
+  const bookSpines = shelf.getElementsByClassName("active");
+  for (const bookSpine of bookSpines) {
+    bookSpine.classList.remove("active");
+  }
+
+  div.classList.add("active");
+
+  bookDetail.classList.remove("hidden");
 }
