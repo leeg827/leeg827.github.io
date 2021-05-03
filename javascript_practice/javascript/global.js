9e93b6c06754b39a08c549a96c18367299dad4be
leeg827 <leeg827@newschool.edu>
2021-05-03 00:17:35 -0700

    garden updates

--- javascript_practice/javascript/global.js
+++ javascript_practice/javascript/global.js
@@ -1,4 +1,3 @@
-let container = document.querySelector('#container')
 let button = document.querySelector('button')
 let count = document.querySelector('#counter')
 let redButton = document.querySelector('#red')
@@ -7,53 +6,55 @@ let yellowButton = document.querySelector('#yellow')
 let clear = document.querySelector('#clear')
 
 button.addEventListener('click',function(){
-   let circle = document.createElement('div');
-   circle.classList.add('circle');
-   circle.style.left=(30*Math.random()) + '%';
-   circle.style.top=(100*Math.random()) + '%';
-   circle.innerHTML='<div></div>';
-   container.appendChild(circle);
+   let flower = document.createElement('div');
+   flower.classList.add('flower');
+   flower.style.left=(100*Math.random()) + '%';
+   flower.style.top=(100*Math.random()) + '%';
+   document.body.appendChild(flower);
 
-   let images = container.querySelectorAll('.circle');
+   let images = document.querySelectorAll('.flower');
    let amount = images.length;
    count.innerHTML = amount;
 });
 
-container.addEventListener ('click',function(event){
-    if(event.target.classList == 'circle'){
+document.body.addEventListener ('click',function(event){
+    if(event.target.classList == 'flower'){
         event.target.remove();
-        let circles= container.querySelectorAll('.circle');
-        let amount = circles.length;
+        let flowers= document.querySelectorAll('.flower');
+        let amount = flowers.length;
         count.innerHTML = amount;
 }});
 
 redButton.addEventListener ('click',function(){
-    let red = document.querySelector('#red').style.backgroundColor;
-    let circles = document.querySelectorAll('.circle');
-    circles.forEach(function(circle){
-        circle.style.backgroundColor = 'red';
+    let flowers = document.querySelectorAll('.flower');
+    flowers.forEach(function(flower){
+        flower.style.background="url('/images/daisy.png')"
+        flower.style.backgroundSize="5rem 5rem";
     })
 });
 
 blueButton.addEventListener ('click',function(){
-    let blue = document.querySelector('#red').style.backgroundColor;
-    let circles = document.querySelectorAll('.circle');
-    circles.forEach(function(circle){
-        circle.style.backgroundColor = 'blue';
+    let flowers = document.querySelectorAll('.flower');
+    flowers.forEach(function(flower){
+        flower.style.background="url('/images/cornflower.png')"
+        flower.style.backgroundSize="5rem 5rem";
     })
 });
 
 yellowButton.addEventListener ('click',function(){
-    let yellow = document.querySelector('#yellow').style.backgroundColor;
-    let circles = document.querySelectorAll('.circle');
-    circles.forEach(function(circle){
-        circle.style.backgroundColor = 'yellow';
+    let flowers = document.querySelectorAll('.flower');
+    flowers.forEach(function(flower){
+        flower.style.background="url('/images/poppy.png')"
+        flower.style.backgroundSize="5rem 5rem";
     })
 });
 
 clear.addEventListener('click',function(){
-    container.innerHTML='';
+    let flowers = document.querySelectorAll('.flower');
+    flowers.forEach((flower)=>{
+        flower.remove();
+    })
     count.innerHTML='0';
-    alert("All clear!");
+    alert("All the flowers are gone!");
 })
 
