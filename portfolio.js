7d89e303f86ff3e84c2ea6d290bf3f88e3b5eceb
leeg827 <leeg827@newschool.edu>
2024-02-08 01:46:20 -0800

    sldkfslk

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,72 @@
-import {gsap} from "gsap";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { TextPlugin } from "gsap/TextPlugin";
-
-
-gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,TextPlugin);
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Your GSAP code here
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
+  // SplitType initialization
+  const myText = new SplitType('#my_text');
+
+  // Disable scrolling
+  document.body.style.overflow = 'hidden';
+
+  // GSAP timeline
+  var tl = gsap.timeline({
+    onComplete: () => {
+      const preloader = document.getElementById('preloader');
+      if (preloader) {
+        preloader.style.display = 'none'; // Hide the preloader element
+        // Enable scrolling after the animation is complete
+        document.body.style.overflow = 'visible';
+      }
+    }
+  });
+
+  // GSAP animations
+  gsap.to('.char', {
+    y: 0,
+    stagger: 0.05,
+    delay: 3.5,
+    duration: 0.1,
+  });
+
+  tl.to('#preloader', {
+    duration: 2,
+  });
+
+  tl.to('#preloader', {
+    duration: 2.5,
+    y: 800,
+    ease: "power2.out",
+  });
+
+  // ScrollTrigger with markers for the '.scrollTrigger' element
+  gsap.to('body', {
+    backgroundColor: '#EAE7DE',
+  });
+  gsap.to('body', {
+    scrollTrigger: {
+      trigger: '.scrollTrigger',
+      markers: true, // Add markers to visualize the trigger
+      start: 'top center',
+      end: 'bottom bottom',
+      toggleActions: "restart none none reverse",
+    },
+    backgroundColor: '#B56135',
+  });
+
+  // ScrollTrigger with markers for the '.footer' element
+  gsap.to('body', {
+    scrollTrigger: {
+      trigger: '.footer',
+      markers: true, // Add markers to visualize the trigger
+      start: 'top center',
+      end: 'bottom bottom',
+      toggleActions: "restart none none reverse",
+    },
+    backgroundColor: '#B0B280',
+  });
+
 });
 
 // Additional custom JavaScript code
